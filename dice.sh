3a3eeeac87a5e2e5d46d1861f978c5ff6c467cdf
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-27 12:05:51 +0530

    dictonary add

--- dice.sh
+++ dice.sh
@@ -1,2 +1,46 @@
 #!/bin/bash -x
 
+declare -A faceValue
+read -p "Enter how many times you want to roll die : " rollDieTimes
+isRoll=0;
+Face_ONE=1;
+FACE_TWO=2;
+FACE_THREE=3;
+FACE_FOUR=4;
+FACE_FIVE=5;
+FACE_SIX=6;
+
+while [[ isRoll -lt rollDieTimes ]]
+do
+        roll=$(( RANDOM%6 + 1 ))
+        case $roll in
+                $FACE_ONE)
+                        face="One"
+                        ;;
+                $FACE_TWO)
+                        face="Two"
+                        ;;
+                $FACE_THREE)
+                        face="Three"
+                        ;;
+                $FACE_FOUR)
+                        face="Four"
+                        ;;
+                $FACE_FIVE)
+                        face="Five"
+                        ;;
+                $FACE_SIX)
+                        face="Six"
+                        ;;
+                *)
+                        face="Unpredictable situation happen"
+                        ;;
+        esac
+        faceValue[$face]=$roll
+        echo $face
+#	echo ${!faceValue[*]}
+        isRoll=$(( $isRoll + 1 ))
+done
+
+echo "${faceValue[@]}"
+echo ${!faceValue[@]}
