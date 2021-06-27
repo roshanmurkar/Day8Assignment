49bf34df1219bb620eaa235f63cd6c969ab8287b
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-27 14:44:02 +0530

    bithday

--- birthMonth.sh
+++ birthMonth.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -x
 
+declare -A Day
+echo "${Day[@]}"
+
+count=1
+while [[ $count -le 50 ]]
+do
+	x=$((RANDOM%12+1))
+	echo "$x"
+	Day[$x]=$((Day[$x]+1))
+	((count++))
+done
+
+echo "Indivisuals having birth date in same month: " ${Day[@]}
